9facc3e6019b1d0afdfd790a57ff3bbe4f19305f
Berzeg07 <berezg86@gmail.com>
2017-08-04 21:58:06 +0300

    4.08

--- index.php
+++ index.php
@@ -7,17 +7,90 @@
 
 	<link rel="stylesheet" href="libs/bootstrap/bootstrap-grid-3.3.1.min.css" />
 	<link rel="stylesheet" href="libs/fancybox/jquery.fancybox.css" />
-		<link rel="stylesheet" href="libs/owl.carousel/assets/owl.carousel.css" />
+	<link rel="stylesheet" href="libs/owl.carousel/assets/owl.carousel.css" />
 	<link rel="stylesheet" href="css/style.css">
 	<link rel="stylesheet" href="css/fonts.css">
-	<link rel="stylesheet" href="css/swiper.min.css">
-	<link rel="stylesheet" href="css/animated-slider.css" />
-	<link rel="stylesheet" href="css/jquery.mCustomScrollbar.css">
+
 	
 </head>
 <body>
 
+	<div class="header">
+		<!-- header-top -->
+		<div class="container">
 
+			<div class="header_top">
+
+				<div class="header_top__item">
+					<img class="img-responsive" src="img/logo.png" alt="">
+				</div>
+
+				<div class="header_top__item">
+					<ul>
+						<li><a href="#">главная</a></li>
+						<li><a href="#">цены</a></li>
+						<li><a href="#">кейсы</a></li>
+						<li><a href="#">education</a></li>
+						<li><a href="#">контакты</a></li>
+					</ul>
+				</div>
+
+				<div class="header_top__item">
+					<a class="number" href="tel:+74998008525">8 <b>(499)</b> 800 85 25</a>
+				</div>
+
+			</div>
+
+		</div>
+
+		<!-- header-bottom -->
+		<div class="container">
+			<div class="row">
+
+				<div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+					<div class="header-box_left">
+						<h1>Студия web-разработки<br/> и интернет маркетинга</h1>
+						<p> За 14 дней увеличим конверсию вашего сайта на 52%
+							с помощью современных технологий привлечения целевого трафика
+							и правильно скомпонованной посадочной страницы.
+						</p>
+						<div class="header-box_left__video">
+							<div class="video-box">
+								<iframe width="100%" height="223" src="https://www.youtube.com/embed/4EoUz39nPMM" frameborder="0" allowfullscreen></iframe>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+					<div class="header-box_right">
+						<div class="header-box_right__phone">
+
+							<div class="header-box_right__container">
+								<div class="header-box_right__item1">
+									<p>Обсудим решение<br/> именно для</p>
+									<p><b>Вашего бизнеса</b></p>
+								</div>
+
+								<form class="form-header" action="#">
+									<input type="text" placeholder="Ваше имя">
+									<input class="phone" type="text" placeholder="Ваш телефон">
+									<button type="submit">хочу новых клиентов</button>
+								</form>
+								<span>Бесплатно</span>
+								<p> создадим макет<br/>
+									вашего будущего сайта
+								</p>
+							</div>
+
+						</div>
+					</div>
+				</div>
+				<div class="clearfix"></div>
+
+			</div>
+		</div>
+
+	</div><!-- header -->
 
 
 
@@ -29,14 +102,10 @@
 	<script src="libs/jquery/jquery-1.11.1.min.js"></script>
 	<script src="libs/owl.carousel/owl.carousel.js"></script>
 	<script src="libs/fancybox/jquery.fancybox.pack.js"></script>
-	<script type="text/javascript" src="js/jquery.mousewheel.js"></script>
-	<script src="js/responsiveTabs.js"></script>
 	<script src="js/jquery.maskedinput.min.js"></script>
 	<script src="js/fonts.js"></script>
 	<script src="js/common.js"></script>
-	<script src="js/swiper.min.js"></script>
-	<script src="js/jquery.cssslider.js"></script>
-	<script src="js/jquery.mCustomScrollbar.concat.min.js"></script>
+
 
 </body>
 </html>
