8bb8f971cc13b44d072258e25418a4976db77173
Berzeg07 <berezg86@gmail.com>
2017-08-06 00:19:43 +0300

    5.08

--- js/common.js
+++ js/common.js
@@ -7,6 +7,58 @@ $(document).ready(function(){
 	});
 	// Конец Мобильное меню
 
+    $(".icons_item").hover(function(e){
+        e.preventDefault();
+        $( ".dark-bg" ).fadeIn(500);
+        
+        // $( ".icons_item span" ).css('color','white');
+        $(this).mouseleave(function(m){
+             m.preventDefault();
+            $( ".dark-bg" ).fadeOut(500); 
+            // $( ".icons_item span" ).css('color','black');   
+        });
+    });
+
+    $(".icons_item1").hover(function(e){
+        $( ".sloi_item__1" ).css('z-index','9999');
+        $(this).mouseleave(function(m){
+              m.preventDefault();
+            $( ".sloi_item__1" ).css('z-index','5');
+         });
+    });
+
+    $(".icons_item2").hover(function(e){
+        $( ".sloi_item__2" ).css('z-index','9999');
+          $(this).mouseleave(function(m){
+              m.preventDefault();
+            $( ".sloi_item__2" ).css('z-index','4');
+        });
+    });
+
+    $(".icons_item3").hover(function(e){
+        $( ".sloi_item__3" ).css('z-index','9999');
+          $(this).mouseleave(function(m){
+              m.preventDefault();
+            $( ".sloi_item__3" ).css('z-index','3');
+        });
+    });
+
+    $(".icons_item4").hover(function(e){
+        $( ".sloi_item__4" ).css('z-index','9999');
+          $(this).mouseleave(function(m){
+              m.preventDefault();
+            $( ".sloi_item__4" ).css('z-index','2');
+        });
+    });
+
+    $(".icons_item5").hover(function(e){
+        $( ".sloi_item__5" ).css('z-index','9999');
+          $(this).mouseleave(function(m){
+              m.preventDefault();
+            $( ".sloi_item__5" ).css('z-index','1');
+        });
+    });
+
     // Fancybox
     $("a[rel=group]").fancybox({
         'transitionIn' : 'none',
